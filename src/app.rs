32ecfd05854e87437b4724539311edf1a9b3e10b
splurf <evandotschwartzentruber@gmail.com>
2026-02-20 06:52:47 -0500

    refac to `app.py`

---
+++ src/app.rs
@@ -0,0 +1,47 @@
+use crate::prelude::*;
+
+pub async fn run(cfg: Config) -> Result<()> {
+    // init application state data
+    let app_state = actix_web::web::Data::new(AppState::new(cfg.cred()).await?);
+
+    actix_web::HttpServer::new(move || {
+        actix_web::App::new()
+            .wrap(actix_web::middleware::Logger::default())
+            .app_data(app_state.clone())
+            .service(
+                actix_web::web::resource(["/rest/getCoverArt", "/rest/getCoverArt.view"])
+                    .route(actix_web::web::get().to(get_cover_art)),
+            )
+            .service(
+                actix_web::web::resource(["/rest/getLicense", "/rest/getLicense.view"])
+                    .route(actix_web::web::get().to(get_license)),
+            )
+            .service(
+                actix_web::web::resource([
+                    "/rest/getOpenSubsonicExtensions",
+                    "/rest/getOpenSubsonicExtensions.view",
+                ])
+                .route(actix_web::web::get().to(get_open_subsonic_extensions)),
+            )
+            .service(
+                actix_web::web::resource(["/rest/getSong", "/rest/getSong.view"])
+                    .route(actix_web::web::get().to(get_song)),
+            )
+            .service(
+                actix_web::web::resource(["/rest/ping", "/rest/ping.view"])
+                    .route(actix_web::web::get().to(ping)),
+            )
+            .service(
+                actix_web::web::resource(["/rest/search3", "/rest/search3.view"])
+                    .route(actix_web::web::get().to(search3)),
+            )
+            .service(
+                actix_web::web::resource(["/rest/stream", "/rest/stream.view"])
+                    .route(actix_web::web::get().to(stream)),
+            )
+    })
+    .bind(cfg.addr())?
+    .run()
+    .await
+    .map_err(Into::into)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod app;
 mod auth;
 mod cfg;
 mod consts;
@@ -7,47 +8,10 @@ mod prelude;
 mod routes;
 mod sink;
 mod state;
-mod util;
-
-use auth::*;
-use cfg::*;
-use consts::*;
-use json::*;
-use opus::*;
-use prelude::*;
-use routes::*;
-use sink::*;
-use state::*;
-use util::*;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> anyhow::Result<()> {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
-
-    // init configurations
-    let cfg = Config::new()?;
-
-    // init application state data
-    let app_state = actix_web::web::Data::new(AppState::new(cfg.cred()).await?);
-
-    actix_web::HttpServer::new(move || {
-        actix_web::App::new()
-            .wrap(actix_web::middleware::Logger::default())
-            .app_data(app_state.clone())
-            .service(ping)
-            .service(get_license)
-            .service(get_open_subsonic_extensions)
-            .service(get_music_folders)
-            .service(get_indexes)
-            .service(get_artists)
-            .service(get_playlists)
-            .service(search3)
-            .service(stream)
-            .service(get_cover_art)
-    })
-    .bind(cfg.addr())?
-    .run()
-    .await?;
-
-    Ok(())
+    let cfg = cfg::Config::new()?;
+    app::run(cfg).await
 }
