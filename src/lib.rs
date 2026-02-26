25f853540db8c96c58f9b1c8b92b4293bf9a5904
splurf <ejsch03@gmail.com>
2026-02-26 09:49:29 -0500

    refac, and for `getauth`

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod auth;
+mod consts;
+mod json;
+mod opus;
+mod prelude;
+mod routes;
+mod sink;
+mod state;
+
+pub mod app;
+pub mod cfg;
+
+pub async fn create_auth() -> anyhow::Result<()> {
+    auth::create_session().await.map(|_| ())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-mod app;
-mod auth;
-mod cfg;
-mod consts;
-mod json;
-mod opus;
-mod prelude;
-mod routes;
-mod sink;
-mod state;
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
-    let cfg = cfg::Config::new()?;
-    app::run(cfg).await
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
+    let cfg = spotisub::cfg::Config::new()?;
+    spotisub::app::run(cfg).await
 }
